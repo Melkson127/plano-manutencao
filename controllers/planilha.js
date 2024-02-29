32949d6c594e22b5c5a460d5c19a297ad31f8dff
Melkson Albuquerque de Moraes <melkson.moraes@lansolver.com>
2024-02-29 09:38:05 -0300

    primeiro commit

---
+++ controllers/planilha.js
@@ -0,0 +1,79 @@
+const Excel = require('exceljs')
+const fs = require('fs')
+
+let workbook = new Excel.Workbook()
+async function extrairDados(path, worksheet) {
+    let reader = await workbook.xlsx.readFile(path)
+    let dados = reader.getWorksheet(worksheet).getSheetValues()
+    return dados
+}
+function createIte(predio, planoManutencao, itemCategoryList) {
+
+    let lines = ['C \n command;subGroup;itemCategory;description;alternativeIdentifier;active;CF_equipamento;CF_LOCAL;CF_LOCAL_DO_ITEM;CF_Periodo;CF_TAG;CF_AREA;CF_tipo_eq']
+    function getPeriodicidade(periodicidade) {
+        if (periodicidade == "Semanal") {
+            return "SE"
+        }
+        return periodicidade.slice(0, 1).toUpperCase()
+    }
+    let tipos = []
+    let identifiers = []
+    let periodicidades = []
+    planoManutencao.forEach((row, i, arr) => {
+        if (i > 1) {
+            let identificacao = row[1]
+            let tipo = row[2]
+            tipos.push(tipo)
+            let pavimento = row[3]
+            let periodicidade = row[4]
+            let itemCategory = ''
+            periodicidade = getPeriodicidade(periodicidade)
+            periodicidades.push(periodicidade)
+            itemCategoryList.forEach((rowCategory, index, array) => {
+                if (index > 1) {
+                    if (tipo == rowCategory[1]) {
+                        itemCategory = rowCategory[2]
+                    }
+                }
+            })
+            let siglaPredio = predio.replace(/ /g, '')
+                .slice(0, 3).toUpperCase()
+            let siglaSubgrupo = tipo.slice(0, 3)
+            let identifier = `${siglaPredio}${siglaSubgrupo}${i - 1}_${periodicidade}`
+            identifiers.push(identifier)
+            lines.push(`I;${tipo};${itemCategory};${identificacao};${identifier};1;${tipo};${pavimento};${predio};${periodicidade};${identifier};${pavimento};${tipo};`)
+        }
+    })
+    return { ite: lines.reduce((val, current) => `${val}\n ${current}`), identifiers: identifiers, tipos: tipos, periodicidades: periodicidades }
+}
+function createIsa(tipos, identifiers, verificacoes, periodicidades) {
+    let lines = ['C \n command;active;order;section;item']
+    tipos.forEach((value, i, arr) => {
+        let section = verificacoes.filter((val, index) => {
+            if (index > 1) {
+                return val[1].trim() == `${value}_${periodicidades[i]}`
+            }
+            return false
+        })
+        lines.push(`I;1;1;${section.length > 0 ? section[0][2] : 'section não encontrada reveja a periodicidade'};${identifiers[i]}`)
+
+    })
+    return lines.reduce((val, current) => `${val}\n ${current}`)
+}
+module.exports = async function create(req, res, next) {
+    let predio = req.body.predio
+    let arquivo = req.file
+    // console.log(req.file)
+    let planoManutencao = await extrairDados(arquivo.path, 'Inventário Cadastro')
+    let itemCategoryList = await extrairDados("./planilha/Planilha_Completa.xlsx", 'itemCategory')
+    let verificacoes = await extrairDados("./planilha/Planilha_Completa.xlsx", 'Verificações')
+
+    // console.log(req.body)
+    let ite = createIte(predio, planoManutencao, itemCategoryList)
+    let isa = createIsa(ite.tipos, ite.identifiers, verificacoes, ite.periodicidades)
+    await fs.rm(arquivo.path, (err) => {
+        console.log(err)
+    })
+    res.render('pronto', { ite: ite.ite, isa: isa })
+    next()
+}
---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+const createItens = require('./controllers/planilha')
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.use(express.static(path.join(__dirname, 'public')))
+const router = express.Router()
+const multer = require('multer')
+// Configuração de armazenamento
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './planilha/')
+    },
+    filename: function (req, file, cb) {
+        // Extração da extensão do arquivo original:
+        const extensaoArquivo = file.originalname.split('.')[1];
+
+        // Indica o novo nome do arquivo:
+        cb(null, `plano-${Date.now()}.${extensaoArquivo}`)
+    }
+});
+const upload = multer({ storage })
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.post('/create', upload.single('plano'), (req, res, next) => createItens(req, res, next))
+router.get('/', (req, res) => {
+    res.render(__dirname + '/views/index.ejs')
+})
+
+app.use(router)
+app.listen(3000, () => {
+    console.log('server running on port 3000')
+})
